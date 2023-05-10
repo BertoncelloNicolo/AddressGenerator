814fbef05fb4a9753586266ac8bcaf34831d3458
Nicolò <nicolo.bertoncello@iisviolamarchesini.edu.it>
2023-05-10 18:09:49 +0200

    classe genera IP

---
+++ GeneraIP/IAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneraIP
+{
+    /// <summary>
+    /// interfaccia da implementare
+    /// </summary>
+    internal interface IAddress
+    {
+        string generateIPv4();
+        string generateSubnet();
+    }
+}
---
+++ GeneraIP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneraIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressGenerator nuovo = new AddressGenerator("11000000.10100000.01010000.10000000");
+             
+            Console.WriteLine($"Indirizzo IP:{nuovo.generateIPv4()}   Subnetmask:{nuovo.generateSubnet()}");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
